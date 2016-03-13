d6afd9b32395e71f5910ee434aff724ffb6a3d7b
zhangshuang_2014@163.com <zhangshuang_2014@163.com>
2016-03-13 22:23:39 +0800

    修改localhost---->gethostbyname

--- conf/config.py
+++ conf/config.py
@@ -1,11 +1,12 @@
 #coding: utf-8
 
-host = '0.0.0.0'
 port = 12345
 #日志文件路径 stdout标准输出
 logfile = 'stdout'
 
 
+import socket
+host = socket.gethostbyname(socket.gethostname())
 appid = '12345'
 userid = '12345'
 appsecret = '12345'
